ce07775c80ae3d9d7b84a6cb2dd972ca40323e30
jairordtgz <jairodri.a05@gmail.com>
2025-11-03 15:45:10 -0500

    crear proyecto de ejemplo

--- App.js
+++ App.js
@@ -1,11 +1,89 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, TouchableHighlight, TouchableOpacity, Pressable } from 'react-native';
+
+// importar imagen local
+import icon from './assets/icon.png'; 
+// const icon = require('./assets/icon.png')
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+    // View es similar a div, view esta en flex
+    // sin componente Text no se puede añadir texto
+
+    <View style={styles.container}> 
+
+      {/* <Image source={icon} style={{ //imagen local
+        width: 100,  //numero de pixeles efectivos en pantalla
+        height: 100,
+        resizeMode: 'center'
+        }}/>   */}
+
+        {/* <Image source={{uri:"https://www.lucasfilm.com/app/uploads/andor-season-2-key-art-poster-480x711.jpg"}} // imagen de la web 
+        style={{
+          width: 350, 
+          height: 600
+        }}/> */}
+      
+       
+      {/* <Text style={{color: 'white'}}>Ya lo pasado pasadoooooo!</Text>
+      <Button 
+      color='red'
+      title='Pulsa aqui' 
+      onPress={() => alert('Hola bro')}
+      />  */}
+      {/* no se puede poner estilos en boton */}
+
+
+      {/* alternativa de boton estilizable */}
+      {/* <TouchableHighlight 
+        underlayColor={"#09f"}
+        onPress={() => alert("Hola guapa de lentes")}
+        style={{
+          width: 200, 
+          height: 200, 
+          backgroundColor: 'red',
+          borderRadius: 50,
+          justifyContent: 'center',
+          alignItems: 'center' 
+        }}
+      > 
+        <Text style={{color: 'white'}}>Ya lo pasado pasadoooooo!</Text>
+      </TouchableHighlight> */}
+
+      {/* <TouchableOpacity // boton estilizable que indica al usuario cuando se pulsa un boton
+        onPress={() => alert("Hola guapa de lentes")}
+        style={{
+          width: 200, 
+          height: 200, 
+          backgroundColor: 'red',
+          borderRadius: 50,
+          justifyContent: 'center',
+          alignItems: 'center' 
+        }}
+      > 
+        <Text style={{color: 'white'}}>Ya lo pasado pasadoooooo!</Text>
+      </TouchableOpacity> */}
+
+
+        <Pressable // componente boton customizable mas recomandado
+          onPress={() => {
+            
+          }}
+          style={({pressed}) => [
+            {
+              backgroundColor: pressed ? 'rgb(210, 230, 255)' : 'white'
+              
+            },
+            styles.wrapperCustom,
+          ]}>
+          {({pressed}) => (
+            <Text style={{fontSize: pressed ? 32: 16}}>{pressed ? 'Pressed!' : 'Press Me'}</Text>
+          )}
+        </Pressable>
+
+
+      <StatusBar eww style="light" />
     </View>
   );
 }
@@ -13,8 +91,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#000', // color fondo
+    alignItems: 'center', // centrar texto
     justifyContent: 'center',
   },
 });
