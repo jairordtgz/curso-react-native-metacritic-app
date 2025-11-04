4ff09ca5d2177f829c42ae5df7179b962024dd1b
jairordtgz <jairodri.a05@gmail.com>
2025-11-03 23:21:48 -0500

    se añadio logo a la parte superior y animacion para cada card de personaje

---
+++ components/CharacterCard.jsx
@@ -0,0 +1,59 @@
+import { useEffect, useRef } from "react";
+import { View, StyleSheet, Text, Image, Animated } from "react-native";
+export function CharacterCard({character}) {
+    return (
+        <View key={character.id} style={styles.card}> 
+        <Image source={{uri: character.image}} style={styles.image}/>
+        <Text style={styles.text}> {character.name} - {character.species}</Text>
+        <Text style={styles.sub}> {character.status} - {character.gender} </Text>
+        </View>
+    )
+}
+
+export function AnimatedCharacterCard({character, index}){
+    const opacity = useRef(new Animated.Value(0)).current; 
+    
+    useEffect(() => {
+        Animated.timing(opacity, {
+            toValue: 1,
+            duration: 500, 
+            delay: index*500,
+            useNativeDriver: true,
+        }).start(); 
+    }, [opacity, index])
+    
+    
+    return (
+        <Animated.View style={{opacity}}> 
+            <CharacterCard character={character}/>
+        </Animated.View>
+    )
+
+}
+
+const styles = StyleSheet.create({
+   scroll: {
+    flexDirection: 'column',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    
+  },
+  card: {
+    margin: 5,
+    marginBottom: 20
+  },
+  image: {
+    width: 120,
+    height: 120,
+    borderRadius: 10,
+  },
+  text: {
+    color: "white",
+    fontSize: 18,
+    marginTop: 8
+  }, 
+  sub: {
+    color: "#aaa",
+    fontSize: 14
+  }
+});
--- components/Main.jsx
+++ components/Main.jsx
@@ -1,10 +1,13 @@
-import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+import { FlatList, StyleSheet, Text, View, Image, ScrollView, ActivityIndicator } from 'react-native';
 import { useEffect, useState } from 'react';
 import { getInfoCharacters } from '../lib/metacritic.js';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { AnimatedCharacterCard, CharacterCard } from './CharacterCard.jsx';
 
 export function Main() {
 
   const [characters, setCharacters] = useState([]); 
+  const insets = useSafeAreaInsets(); //area segura para mostrar contenido
 
   useEffect(() => {
     getInfoCharacters().then(setCharacters)
@@ -12,51 +15,33 @@ export function Main() {
   }, []); 
 
   return (
-    <> 
-      
-      
-      <ScrollView contentContainerStyle={styles.scroll}>
-        {characters.map((character) => (
-          <View key={character.id} style={styles.card}> 
-          <Image source={{uri: character.image}} style={styles.image}/>
-          <Text style={styles.text}> {character.name} - {character.species}</Text>
-          <Text style={styles.sub}> {character.status} - {character.gender} </Text>
-          </View>
-        ))}
-      </ScrollView>
-    </>
+
+    <View style={{paddingTop: insets.top, paddingBottom: insets.bottom}}>
+      <View style={{marginBottom: 20}}>
+        <Image source={{uri:'https://w7.pngwing.com/pngs/277/446/png-transparent-rick-and-morty-illustratrion-rick-sanchez-morty-smith-rick-and-morty-season-1-television-show-youtube-rick-and-morty-television-logo-computer-wallpaper-thumbnail.png'}}
+          style={{width: 50, height: 50}}
+        />
+
+      </View>
+      {characters.length == 0 ? (
+        <ActivityIndicator color={'#fff'} size={'large'}/>
+      ) : (
+
+        // <ScrollView>
+        // {characters.map((character) => (
+        //   <CharacterCard key={character.id} character={character} />
+        // ))}
+        // </ScrollView>
+
+        <FlatList //recomendado para listas largas
+          data={characters}
+          keyExtractor={(character) => character.id}
+          renderItem={({item, index}) => <AnimatedCharacterCard character={item} index={index}/>}
+        
+        /> 
+      )}
+
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000', // color fondo
-    alignItems: 'center', // centrar texto
-    justifyContent: 'center',
-  },
-   scroll: {
-    flexDirection: 'column',
-    flexWrap: 'wrap',
-    justifyContent: 'center',
-    
-  },
-  card: {
-    margin: 5,
-    marginBottom: 20
-  },
-  image: {
-    width: 120,
-    height: 120,
-    borderRadius: 10,
-  },
-  text: {
-    color: "white",
-    fontSize: 18,
-    marginTop: 8
-  }, 
-  sub: {
-    color: "#aaa",
-    fontSize: 14
-  }
-});
